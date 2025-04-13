6a7fb2cbf451a94766c982abfb8c48be6c7a180d
Черепахин Василий <vasi.che@gmail.com>
2025-04-13 19:13:35 +0500

    init db

--- src/main/resources/db/migration/V1_001__create_table.sql
+++ src/main/resources/db/migration/V1_001__create_table.sql
@@ -3,7 +3,7 @@ CREATE table if not exists tutorial
     "n"            serial not null UNIQUE,
     "title"     varchar(120) not null default '',
     "description"      varchar(120) not null default '',
-    "submitted"    varchar(120) not null default '',
+    "submitted"    BOOLEAN not null default false,
     CONSTRAINT tutorial_pkey PRIMARY KEY (n)
 );
 
